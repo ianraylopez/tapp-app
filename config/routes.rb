af2d501bf50a6e00e7cfe57189d9d4fa1d2bfad0
Ian Ray Lopez <ilopez@jway.com>
2015-10-28 15:30:39 +0800

    change Get to Post to function create_user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :users do
         collection do
           get :test
-          get :create_user
+          post :create_user
         end
       end
     end

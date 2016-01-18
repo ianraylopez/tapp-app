fcb14d4209ae2704f791350d4b0bbc40151f07d7
Ian Ray Lopez <ilopez@jway.com>
2016-01-18 21:18:41 +0800

    fixed API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -567,7 +567,7 @@ class API::V1::UsersController < ApplicationController
 		@data = []
 		@dataset = {}
 
-		if @user_apps.blank?
+		if @user_apps == nil
 			@data << @dataset
 		else
 			@user_apps.each do | f |
@@ -602,7 +602,7 @@ class API::V1::UsersController < ApplicationController
 		@data = []
 		@dataset = {}
 
-		if @user_apps.blank?
+		if @user_apps == nil
 			@data << @dataset
 		else
 			@user_apps.each do | f |
